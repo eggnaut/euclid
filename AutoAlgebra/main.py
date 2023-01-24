65dc572e27f3d326806386ce0cb6455481abc081
eggnaut <120997069+eggnaut@users.noreply.github.com>
2023-01-24 11:22:52 -0800

    fixed equation generation and output

--- AutoAlgebra/main.py
+++ AutoAlgebra/main.py
@@ -5,21 +5,26 @@ def generateEq():
     b = f'{randint(endpt1, endpt2)}x'
     c = f'{randint(endpt1, endpt2)}'
 
-    eq = f'{a} + {b} + {c}'
+    eq = f'{a} + {b} + {c} = 0'
+    return eq
 
+eqNum = int(input('Please enter the number of equations you want: '))
 
-eqNum = input('Please enter the number of equations you want: ')
+repeat = True
 
-while endpt1 > 0 and endpt1 >= endpt2:
-    endpt1 = input('Please enter the first endpoint for the range (must be positive, not 0): ')
-    endpt2 = input('Please enter the second endpoint for the range (should be greater than the first endpoint): ')
+while repeat:
+  endpt1 = int(input('\nPlease enter the first endpoint for the range (must be positive, not 0): '))
+  endpt2 = int(input('\nPlease enter the second endpoint for the range (should be greater than the first endpoint): '))
+
+  if endpt1 > 0 and endpt1 < endpt2:
+    repeat = False
 
 equations = []
 
-for x in eqNum:
-    eq = generateEq()
-    equations.append(eq)
+for x in range(eqNum):
+  eq = generateEq()
+  equations.append(eq)
 
 for eq in equations:
-    print(eq)
-    print('\n')
+  print('\n')
+  print(eq)
