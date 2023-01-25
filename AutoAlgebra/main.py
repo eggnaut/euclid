8471584f512a6f65cfa24f7a6501070135524c5a
eggnaut <120997069+eggnaut@users.noreply.github.com>
2023-01-25 10:46:56 -0800

    added option to change the equivalent (usually 0)

--- AutoAlgebra/main.py
+++ AutoAlgebra/main.py
@@ -1,14 +1,19 @@
 from random import randint
 
 def generateEq():
-    a = f'{randint(endpt1, endpt2)}x^2'
-    b = f'{randint(endpt1, endpt2)}x'
-    c = f'{randint(endpt1, endpt2)}'
+  a = f'{randint(endpt1, endpt2)}x^2'
+  b = f'{randint(endpt1, endpt2)}x'
+  c = f'{randint(endpt1, endpt2)}'
 
+  if equiv.lower() == 'yes' or equiv.lower() == 'y':
+    eq = f'{a} + {b} + {c} = 0'
+  elif equiv.lower() == 'no' or equiv.lower() == 'n':
     eq = f'{a} + {b} + {c} = {randint(endpt1, endpt2)}'
-    return eq
+  
+  return eq
 
 eqNum = int(input('Please enter the number of equations you want: '))
+equiv = input('Do you want the equation to be equal to zero?' )
 
 repeat = True
 
