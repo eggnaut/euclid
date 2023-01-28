08f75c2e49040a56d74bf29c6b72cd5927f5bb6d
ALCUNO <db1519@pleasantonusd.net>
2023-01-28 12:05:16 -0800

    fixed bug where font would not work, minor changes

--- AutoAlgebra/generate-eq.py
+++ AutoAlgebra/generate-eq.py
@@ -1,5 +1,7 @@
 from random import randint
 from fpdf import FPDF
+from getpass import getuser
+from platform import system
 
 def generateEq(endpt1: int, endpt2: int, equiv: str):
   a = f'{randint(endpt1, endpt2)}x^2'
@@ -14,13 +16,13 @@ def generateEq(endpt1: int, endpt2: int, equiv: str):
   return eq
 
 eqNum = int(input('Please enter the number of equations you want: '))
-equiv = input('Do you want the equation to be equal to zero?' )
+equiv = input('Do you want the equation to be equal to zero: ' )
 
 repeat = True
 
 while repeat:
-  endpt1 = int(input('\nPlease enter the first endpoint for the range (must be positive, not 0): '))
-  endpt2 = int(input('\nPlease enter the second endpoint for the range (should be greater than the first endpoint): '))
+  endpt1 = int(input('Please enter the first endpoint for the range (must be positive, not 0): '))
+  endpt2 = int(input('Please enter the second endpoint for the range (should be greater than the first endpoint): '))
 
   if endpt1 > 0 and endpt1 < endpt2:
     repeat = False
@@ -33,7 +35,14 @@ for x in range(eqNum):
 
 pdf = FPDF()
 pdf.add_page()
-pdf.set_font('comic sans ms', size = 20)
+
+if system() == 'Darwin':
+  user = getuser()
+  pdf.add_font('csms', '', f'/Users/{user}/Documents/AutoAlgebra/AutoAlgebra/Comic Sans MS.ttf', True)
+elif system() == 'Windows':
+  pdf.add_font('csms', '', r'C:\\Windows\Fonts\Comic.ttf', True)
+
+pdf.set_font('csms', size = 20)
 
 for i in range(len(equations)):
   pdf.cell(200, 10, txt = equations[i - 1], ln = i, align = 'L')
