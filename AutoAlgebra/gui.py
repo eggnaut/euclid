4a428fdd4f2241e75fe804ce4c662f8a00dd6bea
ALCUNO <db1519@pleasantonusd.net>
2023-02-05 10:19:00 -0800

    started transition from tkinter to PySimpleGui

--- AutoAlgebra/gui.py
+++ AutoAlgebra/gui.py
@@ -1,9 +1,23 @@
-from tkinter import *
+from sys import exit
+import PySimpleGUI as sg
 
-root = Tk()
-root.title('AutoAlgebra')
-root.geometry('500x500')
+sg.theme('SystemDefaultForReal')
+font = ('Jost', 20)
 
-quitBtn = Button(root, text = '', command = quit)
+layout = [
+    [sg.B('\u274C', key = '-QUIT-')],
+    [sg.T('Please enter the number of equations you want: '), sg.InputText()],
+    [sg.T('Do you want the equation to be equal to zero (y/n): '), sg.InputText()],
+    [sg.T('Please enter the first endpoint for the range (must be positive, not 0): '), sg.InputText()],
+    [sg.T('Please enter the second endpoint for the range (should be greater than the first endpoint): '), sg.InputText()],
+    [sg.B('\u2705', key = '-SUBMIT-')]
+]
 
-mainloop()
+wn = sg.Window('AutoAlgebra', layout, font = font)
+
+while True:
+    ev, val = wn.read()
+
+    if ev == sg.WIN_CLOSED or ev == '-QUIT-':
+        wn.close()
+        exit()
