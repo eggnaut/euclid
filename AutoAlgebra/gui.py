a41d380712c6f1d266ee22b9ab7b297207705f26
ALCUNO <db1519@pleasantonusd.net>
2023-02-05 10:41:51 -0800

    added button functionality

--- AutoAlgebra/gui.py
+++ AutoAlgebra/gui.py
@@ -5,12 +5,14 @@ sg.theme('SystemDefaultForReal')
 font = ('Jost', 20)
 
 layout = [
-    [sg.B('\u274C', key = '-QUIT-')],
-    [sg.T('Please enter the number of equations you want: '), sg.InputText()],
-    [sg.T('Do you want the equation to be equal to zero (y/n): '), sg.InputText()],
-    [sg.T('Please enter the first endpoint for the range (must be positive, not 0): '), sg.InputText()],
-    [sg.T('Please enter the second endpoint for the range (should be greater than the first endpoint): '), sg.InputText()],
-    [sg.B('\u2705', key = '-SUBMIT-')]
+    [sg.P(), sg.B('Cancel  \u274C', key = '-QUIT-')],
+    [sg.T('')],
+    [sg.T('Please enter the number of equations you want:'), sg.P(), sg.InputText(key = '-NUM-')],
+    [sg.T('Do you want the equation to be equal to zero (y/n):'), sg.P(), sg.InputText(key = '-EQUIV-')],
+    [sg.T('Please enter the first endpoint for the range (must be positive, not 0):'), sg.P(), sg.InputText(key = '-ENDPT1-')],
+    [sg.T('Please enter the second endpoint for the range (should be greater than the first endpoint):'), sg.P(), sg.InputText(key = '-ENDPT2-')],
+    [sg.T('')],
+    [sg.P(), sg.B('Submit  \u2705', key = '-SUBMIT-')]
 ]
 
 wn = sg.Window('AutoAlgebra', layout, font = font)
@@ -21,3 +23,9 @@ while True:
     if ev == sg.WIN_CLOSED or ev == '-QUIT-':
         wn.close()
         exit()
+
+    if ev == '-SUBMIT-':
+        num = val['-NUM-']
+        equiv = val['-EQUIV-']
+        endpt1 = val['-ENDPT1-']
+        endpt2 = val['-ENDPT2-']
