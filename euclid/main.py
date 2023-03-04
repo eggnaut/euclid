feedcda558ace8c803a4472405258e30163429cb
ALCUNO <db1519@pleasantonusd.net>
2023-03-03 18:01:28 -0800

    added import safeguard

--- euclid/main.py
+++ euclid/main.py
@@ -123,68 +123,69 @@ def generatePDF():
         pdf.output(f'{pdfPath}\\QuadraticEqs.pdf')
 
 # main loop for the PySimpleGUI app
-while True:
-    ev, val = wn.read()
+if __name__ == '__main__':
+    while True:
+        ev, val = wn.read()
 
-    if ev == sg.WIN_CLOSED or ev == '-QUIT-':
-        wn.close()
-        exit()
+        if ev == sg.WIN_CLOSED or ev == '-QUIT-':
+            wn.close()
+            exit()
 
-    # if user clicks submit, then generate equations
-    if ev == '-SUBMIT-':
-        num = int(val['-NUM-'])
-        equiv = val['-EQUIV-']
-        endpt1 = int(val['-ENDPT1-'])
-        endpt2 = int(val['-ENDPT2-'])
+        # if user clicks submit, then generate equations
+        if ev == '-SUBMIT-':
+            num = int(val['-NUM-'])
+            equiv = val['-EQUIV-']
+            endpt1 = int(val['-ENDPT1-'])
+            endpt2 = int(val['-ENDPT2-'])
 
-        # error message for endpoints
-        if not (endpt1 > 0 and endpt2 > endpt1):
-            sg.popup_no_buttons(
-                f'Please check that the first endpoint is greater than 0.\n\nPlease check that the second endpoint is greater than the first endpoint.',
-                font = font,
-                title = 'Error \u274C',
-                keep_on_top = True,
-                auto_close = True,
-                auto_close_duration = 4
-            )
-        
-        # error message for number of equations
-        if num <= 0:
-            sg.popup_no_buttons(
-                f'Please check that the number of equations requested is greater than 0.',
-                font = font,
-                title = 'Error \u274C',
-                keep_on_top = True,
-                auto_close = True,
-                auto_close_duration = 4
-            )
-
-        # error message for incorrect input
-        if not (equiv.lower() == 'y' or equiv.lower() == 'yes' or equiv.lower() == 'n' or equiv.lower() == 'no'):
-            sg.popup_no_buttons(
-                f'Please make sure you entered \'y\' or \'n\' for the second question:\n\n\'Do you want the equation to be equal to zero (y/n)\'',
-                font = font,
-                title = 'Error \u274C',
-                keep_on_top = True,
-                auto_close = True,
-                auto_close_duration = 4
-            )
-
-        # actually start generating equations and PDF
-        else:
-            equations = []
-            for x in range(num):
-                eq = generateEq(endpt1, endpt2, equiv)
-                equations.append(eq)
-
-            generatePDF()
-
-            # success message
-            sg.popup_no_buttons(
-                f'The PDF was saved in your Downloads folder here:\n\n{pdfPath}/QuadraticEqs.pdf\n\nYou can now quit the app.', 
-                font = font, 
-                title = 'Success \u2705', 
-                keep_on_top = True, 
-                auto_close = True, 
-                auto_close_duration = 4
+            # error message for endpoints
+            if not (endpt1 > 0 and endpt2 > endpt1):
+                sg.popup_no_buttons(
+                    f'Please check that the first endpoint is greater than 0.\n\nPlease check that the second endpoint is greater than the first endpoint.',
+                    font = font,
+                    title = 'Error \u274C',
+                    keep_on_top = True,
+                    auto_close = True,
+                    auto_close_duration = 4
                 )
+            
+            # error message for number of equations
+            if num <= 0:
+                sg.popup_no_buttons(
+                    f'Please check that the number of equations requested is greater than 0.',
+                    font = font,
+                    title = 'Error \u274C',
+                    keep_on_top = True,
+                    auto_close = True,
+                    auto_close_duration = 4
+                )
+
+            # error message for incorrect input
+            if not (equiv.lower() == 'y' or equiv.lower() == 'yes' or equiv.lower() == 'n' or equiv.lower() == 'no'):
+                sg.popup_no_buttons(
+                    f'Please make sure you entered \'y\' or \'n\' for the second question:\n\n\'Do you want the equation to be equal to zero (y/n)\'',
+                    font = font,
+                    title = 'Error \u274C',
+                    keep_on_top = True,
+                    auto_close = True,
+                    auto_close_duration = 4
+                )
+
+            # actually start generating equations and PDF
+            else:
+                equations = []
+                for x in range(num):
+                    eq = generateEq(endpt1, endpt2, equiv)
+                    equations.append(eq)
+
+                generatePDF()
+
+                # success message
+                sg.popup_no_buttons(
+                    f'The PDF was saved in your Downloads folder here:\n\n{pdfPath}/QuadraticEqs.pdf\n\nYou can now quit the app.', 
+                    font = font, 
+                    title = 'Success \u2705', 
+                    keep_on_top = True, 
+                    auto_close = True, 
+                    auto_close_duration = 4
+                    )
